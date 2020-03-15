41be5229f0d588e876a5bfbe84539b4d50c6e06e
KGR0903 <kgowthamrajkgr@gmail.com>
2020-03-15 11:37:45 +0530

    myproject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-springboot';
+  title = 'Angular 6 + Spring Boot 2 + Spring Data JPA + MySQL + CRUD Tutorial';
+
 }
